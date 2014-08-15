59b4e086b0ba03ae8d1bed9068a55e37cc845567
DuyNT9 <DuyNT9@301DUYNT9.fsoft.fpt.vn>
2014-08-15 16:51:01 +0700

    chaned

--- Tutorial/src/codility/chap8/PeaksSolution.java
+++ Tutorial/src/codility/chap8/PeaksSolution.java
@@ -2,6 +2,8 @@ package codility.chap8;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.Test;
 
 public class PeaksSolution {
@@ -16,23 +18,59 @@ public class PeaksSolution {
 		System.out.println(A[A.length - 1] + "}");
 	}
 
-	public int findNumberPeas(int[] A) {
-		printArr(A);
-		int count = 0;
-		for (int i = 0; i < A.length - 2; i++) {
-			if (A[i + 1] > A[i] && A[i + 1] > A[i + 2]) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	public int solution(int[] A) {
-		int numPeas = findNumberPeas(A);
-
-		// System.out.println(numPeas);
-		return numPeas;
-	}
+	public ArrayList<Integer> findDevisor(int n, int max_size)
+    {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        int root = (int) Math.sqrt(n);
+        for(int i=2; i<=root; i++) {
+            if (n%i == 0)
+            {
+                int b = n / i;
+                if (i <= max_size)
+                    arrayList.add(i);
+                if (b <= max_size)
+                    arrayList.add(b);
+            }
+        }
+        Collections.sort(arrayList);
+        return arrayList;
+    }
+    
+    public ArrayList<Integer> findPeaks(int[] A)
+    {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        
+        for(int i=1; i<A.length-1; i++)
+        {
+            if (A[i] > A[i-1] && A[i] > A[i+1])
+            {
+                arrayList.add(i);
+            }
+        }
+        return arrayList;
+    }
+    
+    public int findMaxDivided(int[] A, int n)
+    {
+        return 0;
+    }
+    
+    public int solution(int[] A) {
+        ArrayList<Integer> peaks = findPeaks(A);
+        ArrayList<Integer> divisors = findDevisor(A.length, peaks.size());
+        
+        for(Integer d : divisors)
+        {
+            System.out.println(d);
+        }
+        
+        System.out.println(";;--;;");
+        for(Integer d : peaks)
+        {
+            System.out.println(d);
+        }
+        return 0;
+    }
 
 	@Test
 	public void testCase1() throws Exception {
--- Tutorial/src/progpearls/chap2/P3Anagram.java
+++ Tutorial/src/progpearls/chap2/P3Anagram.java
@@ -5,25 +5,63 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class P3Anagram {
-	
-	public static <T> String join(T[] arr)
-	{
+
+	public static String join(int[] arr) {
 		StringBuilder buidler = new StringBuilder();
 		for (int i = 0; i < arr.length; i++) {
-			T c = arr[i];
+			int c = arr[i];
 			buidler.append(c);
 		}
 		return buidler.toString();
 	}
 	
-	public void rearrange(char[] arr)
-	{
-		//String s = join<String>(new String[]{});
-		//System.out.println(s);
+	static int count = 1;
+
+	public void reArrange(int[] arr, int base, int end) {
+		System.out.println(count++ + ". " + join(arr));
+		int last = arr[end];
+		int i = 0;
+		for(i=end-1; i >= 0 && (arr[i+1] < arr[i] || arr[i] > last) ;i--)
+		{
+			arr[i+1] = arr[i];
+		}
+		
+		//System.out.println(join(arr));
+		//System.out.println(i);
+		
+		if (i > 0)
+		{
+			arr[i+1] = arr[i];
+			arr[i] = last;
+			//System.out.println(join(arr));
+			reArrange( arr, base, end);
+		}
 	}
 	
+	public void reArrange(int[] arr) {
+		reArrange(arr, arr.length-1, arr.length-1);
+	}
+
 	@Test
 	public void testCase1() throws Exception {
-		rearrange("abcd".toCharArray());
+		//1. 123456
+		//2. 123465
+		//3. 123546
+		//4. 123564
+		//5. 124356
+		
+		//...
+		//654321
+		
+//		1. 123456
+//		2. 123465
+//		3. 123546
+//		4. 123564
+//		5. 123456
+//		6. 123465
+//		7. 123546
+//		8. 123564
+//		9. 123456
+		reArrange(new int[] { 1, 2, 3, 4, 5, 6 });
 	}
 }
