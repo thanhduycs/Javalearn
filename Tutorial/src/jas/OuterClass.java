d37884c0106e97c90263b773245254dd0e1787e9
DuyNT9 <DuyNT9@301DUYNT9.fsoft.fpt.vn>
2014-08-18 16:38:53 +0700

    first 3 exercises in chapter 2 of programming pearls

---
+++ Tutorial/src/jas/OuterClass.java
@@ -0,0 +1,11 @@
+package jas;
+
+public class OuterClass {
+	public class InnerClass {
+		public String inner = "I'm inner";
+	}
+
+	public static class InnerStaticClass {
+		public String innerStatic = "I'm innerStatic";
+	}
+}
