483e0152a577717d5ea2a196bc909e2afb344f42
DuyNT9 <DuyNT9@301DUYNT9.fsoft.fpt.vn>
2014-08-29 14:45:09 +0700

    not completed PeakSolution2, using Binary Search.

--- Tutorial/src/codility/chap8/PeaksSolution2.java
+++ Tutorial/src/codility/chap8/PeaksSolution2.java
@@ -8,189 +8,93 @@ import org.junit.Test;
 
 public class PeaksSolution2 {
 
-	public void printArr(int[] A, String caption) {
-		if (A.length > 100)
-			return;
-		System.out.printf(caption + " {%d,", A[0]);
-		for (int i = 1; i < A.length - 1; i++) {
-			System.out.printf("%2d, ", A[i]);
-		}
-		System.out.printf("%2d}\n", A[A.length - 1]);
-	}
+    public void printArr(int[] A, String caption) {
+        if (A.length > 100)
+            return;
+        System.out.printf(caption + " {%d,", A[0]);
+        for (int i = 1; i < A.length - 1; i++) {
+            System.out.printf("%2d, ", A[i]);
+        }
+        System.out.printf("%2d}\n", A[A.length - 1]);
+    }
 
-	public int[] findDivisor(int n, int max_size) {
-		int[] array = new int[n];
-		int root = (int) Math.sqrt(n);
-		int i = 0;
-		for (int divisor = 2; divisor <= root; divisor++) {
-			if (n % divisor == 0) {
-				int b = n / divisor;
-				if (divisor >= n / max_size)
-					array[i++] = divisor;
-				if (b >= n / max_size)
-					array[i++] = b;
-			}
-		}
-		return Arrays.copyOf(array, i);
-	}
+    private boolean checkSol(int[] prefix_sum, int nblock, final int N) {
+        if (nblock == 0){
+            System.out.println();
+        }
+        int nsize = N / nblock;
+        int i = 0;
+        while (i < N) {
+            int a = i == 0 ? 0 : i - 1;
+            int b = i + nsize;
 
-	public int[] findPeaks(int[] A) {
-		int[] array = new int[A.length / 2];
-		int i = 0;
-		for (int p = 1; p < A.length - 1; p++) {
-			if (A[p] > A[p - 1] && A[p] > A[p + 1]) {
-				array[i++] = p;
-			}
-		}
-		return Arrays.copyOf(array, i);
-	}
+            if (prefix_sum[b] - prefix_sum[a] <= 0)
+                return false;
+            i += nsize;
+        }
+        return true;
+    }
 
-	public int findMaxDivided(int[] A, int n) {
-		return 0;
-	}
+    public int[] findPossibeBlocks(int n, int max_value) {
+        int[] array = new int[n];
+        int root = (int) Math.sqrt(n);
+        int i = 0;
+        for (int divisor = 1; divisor <= root && divisor <= max_value; divisor++) {
+            if (n % divisor == 0) {
+                array[i++] = divisor;
+            }
+        }
+        return Arrays.copyOf(array, i);
+    }
 
-	public int k(int[] A) {
+    private int binarySol(final int N, int[] prefix_sum, int[] block_sizes,
+            int begin, int end) {
+        if (end - begin < 2) {
+            if (checkSol(prefix_sum, block_sizes[end], N))
+                return block_sizes[end];
+            return block_sizes[begin];
+        }
 
-		System.out.println("LENGTH = " + A.length);
-		int[] peaks = findPeaks(A);
+        int mid = (begin + end) / 2;
+        int nblock = block_sizes[mid];
+        boolean ok = checkSol(prefix_sum, nblock, N);
 
-		if (peaks.length == 0) {
-			return 0;
-		}
+        if (ok) {
+            return binarySol(N, prefix_sum, block_sizes, mid, end);
+        } else {
+            return binarySol(N, prefix_sum, block_sizes, begin, mid);
+        }
+    }
 
-		int[] divisors = findDivisor(A.length, peaks.length);
+    public int solution(int[] A) {
+        int[] prefix_sum = new int[A.length+1];
+        int peak_count = 0;
+        for (int p = 1; p < A.length - 1; p++) {
+            if (A[p] > A[p - 1] && A[p] > A[p + 1]) {
+                peak_count++;
+            }
+            prefix_sum[p] = peak_count;
+        }
+        prefix_sum[A.length-1] = peak_count;
+        prefix_sum[A.length] = peak_count;
+        int[] block_sizes = findPossibeBlocks(A.length, peak_count);
+        return binarySol(A.length, prefix_sum, block_sizes, 1, peak_count);
+    }
 
-		Arrays.sort(divisors);
+    @Test
+    public void testCase1() throws Exception {
+        int[] arr = { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
+        int result = solution(arr);
 
-		boolean[] markers = new boolean[A.length];
-		for (Integer d : peaks) {
-			markers[d] = true;
-		}
+        assertEquals(3, result);
+    }
 
-		for (int i = 0; i < markers.length; i++) {
-			System.out.printf(" %d ", i%10);
-		}
-		System.out.println();
-
-		int[] prefix_sum = new int[A.length];
-		int count = 0;
-		for (int i = 0; i < markers.length; i++) {
-			boolean b = markers[i];
-			if (b)
-				count++;
-			prefix_sum[i] = count;
-			System.out.printf("[%c]", b ? '*' : ' ');
-		}
-		System.out.println();
-
-		for (int i = 0; i < prefix_sum.length; i++) {
-			int j = prefix_sum[i];
-			System.out.printf("[%d]", j);
-		}
-		System.out.println();
-
-		if (divisors.length == 0) {
-			return 1;
-		}
-
-		printArr(divisors, "DIVISORS: ");
-		printArr(peaks, "PEAKS:    ");
-
-		int result = 1;
-
-		for (int i = 0; i < divisors.length; i++) {
-			int size = divisors[i]; //
-			int x = 0;
-			boolean flag = true;
-			while (x < prefix_sum.length) {
-				int a = x;
-				int b = x + size - 1;
-				int num = x == 0 ? prefix_sum[b] : prefix_sum[b]
-						- prefix_sum[a - 1];
-				System.out.printf("(%d, %d) = %d\n", a, b, num);
-				x += size;
-				if (num == 0) {
-					flag = false;
-					break;
-				}
-			}
-
-			if (flag) {
-				result = A.length / size;
-				break;
-			}
-		}
-		return result;
-	}
-
-	public int[] decomposeNumber1(int number) {
-		int[] results = new int[100];
-		int length = 0;
-		int k = 2;
-		while (number > 1) {
-			if (number % k == 0) {
-				results[length++] = k;
-				number = number / k;
-			} else {
-				k = k + 1;
-			}
-		}
-		return Arrays.copyOf(results, length);
-	}
-
-	public int[] decomposeNumber2(int number) {
-		if (number <= 2) {
-			return new int[] { number };
-		}
-		int[] results = new int[100];
-		int length = 0;
-		int k = 2;
-		double root = Math.sqrt(number);
-		while (number > 1) {
-			if (number % k == 0) {
-				results[length++] = k;
-				number = number / k;
-				root = Math.sqrt(number);
-			} else {
-				k = PrimeNumberHelper.next(k);
-				if (k > root)
-				{
-					results[length++] = k;
-					break;
-				}
-			}
-		}
-		return Arrays.copyOf(results, length);
-	}
-	
-	int split(int[] divisors, int begin, int end)
-	{
-		return 0;
-	}
-
-	public int solution(int[] A) {
-		int[] peaks = findPeaks(A);
-		int[] divisors = decomposeNumber2(A.length);
-		
-		printArr(divisors, "DIVISORS:");
-
-		return 0;
-	}
-
-	@Test
-	public void testCase1() throws Exception {
-		int[] arr = { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
-		int result = solution(arr);
-
-		assertEquals(3, result);
-	}
-
-	// @Test
-	// public void testCase2() throws Exception {
-	// int[] arr = { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0,
-	// 1, 1, 0, 0, 1, 0, 1, 0 };
-	// int result = solution(arr);
-	//
-	// assertEquals(result, 3);
-	// }
+    // @Test
+    // public void testCase2() throws Exception {
+    // int[] arr = { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0,
+    // 1, 1, 0, 0, 1, 0, 1, 0 };
+    // int result = solution(arr);
+    //
+    // assertEquals(result, 3);
+    // }
 }
