061b26218fcc0677597ab8ed9421e47b527e6938
Duy Nguyen <thanhduy.cs@gmail.com>
2014-09-01 22:27:28 +0700

    changed

--- Tutorial/src/codility/chap11/Ladder.java
+++ Tutorial/src/codility/chap11/Ladder.java
@@ -1,5 +1,7 @@
 package codility.chap11;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 @SuppressWarnings("unused")
@@ -14,7 +16,7 @@ public class Ladder {
 
     public int[] solution(int[] A, int[] B) {
         // Elements of B have value in range[1-30]
-        int[] power2 = new int[31];
+        final int[] power2 = new int[31];
         power2[0] = 1;
         for (int i = 1; i < power2.length; i++) {
             power2[i] = power2[i - 1] * 2;
@@ -27,7 +29,7 @@ public class Ladder {
 
         for (int i = 2; i <= B.length; i++) {
             fibos[i] = fibos[i - 1] + fibos[i - 2];
-            fibos[i] = fibos[i] % power2[30]; //!important
+            fibos[i] = fibos[i] % power2[30]; // !important
         }
 
         int[] results = new int[B.length];
@@ -44,5 +46,31 @@ public class Ladder {
 
         int[] results = solution(A, B);
         printArr(results, "results");
+    }
+
+    @Test
+    public void testModulo() throws Exception {
+        final int[] power2 = new int[31];
+        power2[0] = 1;
+        for (int i = 1; i < power2.length; i++) {
+            power2[i] = power2[i - 1] * 2;
+            // System.out.println(i + " => " + power2[i]);
+        }
+
+        long num = 0;
+        int p = 0;
+        
+        final int MAX_P = 10;
+
+        for (int i = 0; i < 100000; i++) {
+            num = (long) (Math.random() * 11L * power2[MAX_P]);
+            p = (int) (Math.random() * 10000) % MAX_P + 1;
+
+            long a = num % power2[p];
+            long b = num % power2[MAX_P];
+            b = b % power2[p];
+
+            assertEquals(a, b);
+        }
     }
 }
