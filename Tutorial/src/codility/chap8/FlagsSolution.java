297a91818c635e8dce3314676d3053466e016c71
DuyNT9 <DuyNT9@301DUYNT9.fsoft.fpt.vn>
2014-09-03 16:55:05 +0700

    finish chapter 8

--- Tutorial/src/codility/chap8/FlagsSolution.java
+++ Tutorial/src/codility/chap8/FlagsSolution.java
@@ -3,77 +3,68 @@ package codility.chap8;
 import java.util.Arrays;
 
 public class FlagsSolution {
-	public void printArr(int[] A, String caption) {
-		if (A.length > 100)
-			return;
-		System.out.printf(caption + " {%d,", A[0]);
-		for (int i = 1; i < A.length - 1; i++) {
-			System.out.printf("%2d, ", A[i]);
-		}
-		System.out.printf("%2d}\n", A[A.length - 1]);
-	}
+    public void printArr(int[] A, String caption) {
+        if (A.length > 100)
+            return;
+        System.out.printf(caption + " {%d,", A[0]);
+        for (int i = 1; i < A.length - 1; i++) {
+            System.out.printf("%2d, ", A[i]);
+        }
+        System.out.printf("%2d}\n", A[A.length - 1]);
+    }
 
-	public int[] findPeaks(int[] A) {
-		int[] array = new int[A.length / 2];
-		int i = 0;
-		for (int p = 1; p < A.length - 1; p++) {
-			if (A[p] > A[p - 1] && A[p] > A[p + 1]) {
-				array[i++] = p;
-			}
-		}
-		return Arrays.copyOf(array, i);
-	}
+    public int[] findPeaks(int[] A) {
+        int[] array = new int[A.length / 2];
+        int i = 0;
+        for (int p = 1; p < A.length - 1; p++) {
+            if (A[p] > A[p - 1] && A[p] > A[p + 1]) {
+                array[i++] = p;
+            }
+        }
+        return java.util.Arrays.copyOf(array, i);
+    }
 
-	public int solution(int[] A) {
-		int[] peaks = findPeaks(A);
-		printArr(peaks, "PEAKS:  ");
+    public boolean trySolution(int[] A, int[] peaks, final int tryMidSolution) {
+        int distance = 0;
+        int count = 1;
+        for (int i = 1; i < peaks.length; i++) {
+            distance += peaks[i] - peaks[i - 1];
+            if (distance >= tryMidSolution) {
+                count++;
+                distance = 0;
+            }
+        }
+        return count >= tryMidSolution;
+    }
 
-		boolean[] markers = new boolean[A.length];
-		for (int i = 0; i < peaks.length; i++) {
-			markers[peaks[i]] = true;
-		}
+    public int binarySearch(int[] A, int[] peaks, int begin, int end) {
+        if (end - begin < 2) {
+            if (trySolution(A, peaks, end))
+                return end;
+            return begin;
+        }
 
-		int count = 0;
-		int[] prefix_sum = new int[A.length];
-		for (int i = 0; i < markers.length; i++) {
-			boolean b = markers[i];
-			if (b)
-				count++;
-			prefix_sum[i] = count;
-		}
+        int mid = (begin + end) / 2;
+        if (trySolution(A, peaks, mid)) {
+            return binarySearch(A, peaks, mid, end);
+        } else {
+            return binarySearch(A, peaks, begin, mid);
+        }
+    }
 
-		printArr(prefix_sum, "PREFIX: ");
+    public int solution(int[] A) {
+        int[] peaks = findPeaks(A);
+        if (peaks.length <= 1)
+            return peaks.length;
+        int max_possible_flag = (int)Math.min(peaks.length, Math.sqrt(A.length)) + 1;
+        int max_flag = binarySearch(A, peaks, 1, max_possible_flag);
+        return max_flag;
+    }
 
-		int[] differents = new int[peaks.length];
+    public static void main(String[] args) {
+        FlagsSolution flagsSolution = new FlagsSolution();
+        int[] arr = new int[] { 1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
 
-		int mark = peaks[0];
-		int min_diff = Integer.MAX_VALUE;
-
-		for (int i = 1; i < peaks.length; i++) {
-			int t = peaks[i] - mark;
-			min_diff = Math.min(t, min_diff);
-			mark = peaks[i];
-			differents[i] = t;
-		}
-		
-		int max_flag = peaks.length;
-		
-		while(max_flag > 1)
-		{
-			int size = (int)(A.length / (double)max_flag);
-		}
-		
-		
-		printArr(differents, "DIFF:   ");
-
-		System.out.println(min_diff);
-		return max_flag;
-	}
-
-	public static void main(String[] args) {
-		FlagsSolution flagsSolution = new FlagsSolution();
-		int[] arr = new int[] { 1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
-
-		flagsSolution.solution(arr);
-	}
+        flagsSolution.solution(arr);
+    }
 }
